5ba02c1d5d658f5dfc941b5164b90d445b90a03f
Kamryn Smith <programersign@gmail.com>
2023-07-10 10:04:57 -0700

    established player class

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,3 +1,11 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, Metadata #
 from sqlalchemy.orm import relationship, backref #
 from sqlalchemy.ext.declarative import declarative_base #
+
+# metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+
+class Player(Base):
+    
+
