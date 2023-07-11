a96ca228d65cc0e2085a4d130141f461d6e20364
Kamryn Smith <programersign@gmail.com>
2023-07-11 11:31:24 -0700

    added seeding for players table

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -8,4 +8,21 @@ from sqlalchemy.orm import sessionmaker
 from models import Player, Card, Deck
 
 if __name__ == '__main__':
-    
+    engine = create_engine('sqlite:///collection.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # add delete
+
+    fake = Faker()
+
+    players = []
+    for i in range(50):
+        player = Player(
+            name=fake.unique.name()
+        )
+
+        session.add(player)
+        session.commit()
+
+        players.append(player)
