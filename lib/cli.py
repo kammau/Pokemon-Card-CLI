2f99975f5850dbdeeea773b311196f097a6d639e
Kamryn Smith <programersign@gmail.com>
2023-07-15 15:32:50 -0700

    added options to main menu and if elif else

--- lib/cli.py
+++ lib/cli.py
@@ -19,24 +19,14 @@ class PokemonCli:
         user = input("Please enter your username: ") #decide between username and name!
         usernames = [name.username for name in self.player]
         if user in usernames:
-            print(f"Welcome back {user}!")
-            self.main_menu()
+            print("")
+            self.main_menu(user)
         else:
             sign_up = input("Looks like your new here, do you want to sign up? (y/n) ")
             if sign_up == "y":
                 self.sign_up()
             elif sign_up == "n":
                 print("Ok, have a good day!")
-        # for player in self.player:
-        #     if user == player.username:
-        #         print(f"Welcome back {user}!")
-        #         self.main_menu()
-        #     else:
-        #         sign_up = input("Looks like your new here, do you want to sign up? (y/n) ")
-        #         if sign_up == "y":
-        #             self.sign_up()
-        #         elif sign_up == "n":
-        #             print("Ok, have a good day!")
 
     
     def sign_up(self):
@@ -44,8 +34,22 @@ class PokemonCli:
         level = input("Please specify a playing level (Beginner, Intermediate, Advanced): ")
         add_data(Player(level=level, username=username))
 
-    def main_menu(self):
-        print("What you wanna do?")
+    def main_menu(self, user): #***COME BACK TO ADD UPDATE (crUd)***
+        print(f"Welcome back {user}! Please select an option:")
+        options = input('1) Look through collection \n'
+            '2) Add new item to collection \n'
+            '3) Remove an item from your collection \n'
+            '4) ')
+        
+        if options == "1":
+            self.read_collection()
+        elif options == "2":
+            self.add_to_collection()
+        elif options == "3":
+            self.delete_from_collection()
+        else:
+            print("Please type a number!")
+        
 
 if __name__ == "__main__":
     engine = create_engine("sqlite:///db/cards.db") # home base for the database
